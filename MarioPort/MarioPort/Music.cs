a3b3d0b227127704b97321bb818598d28691dc9e
unknown <UWP_User@ULR009-12.(none)>
2012-04-19 16:29:54 -0500

    putting last files in Signed-off-by: unknown <UWP_User@ULR009-12.(none)>

--- MarioPort/MarioPort/Music.cs
+++ MarioPort/MarioPort/Music.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+//using Buffers;
+//using Crt;
+
+
 namespace MarioPort
 {
    public static class Music
    {
 
+         public const double HALF_NOTE = 1.059463094;
+         public const int MAX_OCT = 7;
+
+          //rTmp: Real;
+         public static int[] aiNote = new int[MAX_OCT * 12];
+         public static int i;
+         public static string sMusic;
+         public static int iPos;
+
+        public static void StartMusic(string S)
+        {
+
+        }
+
+        public static void PlayMusic()
+        {
+           
+        }
+
+        public static void StopMusic()
+        {
+           
+        }
+
+        public static void PauseMusic()
+        {
+           
+        }
    }
+
 }
